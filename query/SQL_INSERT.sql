900eff03dd08cb0d7a5cd3d0f51cf8354fd183c5
vorobievka <132885907+vorobievka@users.noreply.github.com>
2023-12-16 13:54:33 +0300

    Update SQL_INSERT.sql
    v3

--- query/SQL_INSERT.sql
+++ query/SQL_INSERT.sql
@@ -1,53 +1,31 @@
 INSERT INTO public.artist
 ("name")
-VALUES('best artist1');
-
-INSERT INTO public.artist
-("name")
-VALUES('artist2');
-
-INSERT INTO public.artist
-("name")
-VALUES('good artist3');
-
-INSERT INTO public.artist
-("name")
-VALUES('artist4');  
+VALUES
+('best artist1'),
+('artist2'),
+('good artist3'),
+('artist4');
 
 
 INSERT INTO public.genre
 ("title")
-VALUES('Pop');
-
-INSERT INTO public.genre
-("title")
-VALUES('Rock');
-
-INSERT INTO public.genre
-("title")
-VALUES('Jazz');
-
-INSERT INTO public.genre
-("title")
-VALUES('Trot');
+VALUES
+('Pop'),
+('Rock'),
+('Jazz'),
+('Trot');
 
 
 INSERT INTO public.album
 (title, "year")
-
 VALUES('album1', 2020);
 
 INSERT INTO public.song
 ("name", duration, album_id)
-VALUES('song1_1', 192, 1);
-
-INSERT INTO public.song
-("name", duration, album_id)
-VALUES('song2_1', 112, 1);
-
-INSERT INTO public.song
-("name", duration, album_id)
-VALUES('song3_1', 162, 1);
+VALUES
+('song1_1', 192, 1),
+('song2_1', 112, 1),
+('song3_1', 162, 1);
 
 
 INSERT INTO public.album
@@ -56,11 +34,9 @@ VALUES('album2', 2019);
 
 INSERT INTO public.song
 ("name", duration, album_id)
-VALUES('my_song1_2', 295, 2);
-
-INSERT INTO public.song
-("name", duration, album_id)
-VALUES('song2_2', 225, 2);
+VALUES
+('my_song1_2', 295, 2),
+('song2_2', 225, 2);
 
 
 INSERT INTO public.album
@@ -69,11 +45,9 @@ VALUES('album3', 2005);
 
 INSERT INTO public.song
 ("name", duration, album_id)
-VALUES('song1_3', 142, 3);
-
-INSERT INTO public.song
-("name", duration, album_id)
-VALUES('song2_3', 252, 3);
+VALUES
+('song1_3', 142, 3),
+('song2_3', 252, 3);
 
 
 INSERT INTO public.album
@@ -82,96 +56,47 @@ VALUES('album4', 2015);
 
 INSERT INTO public.song
 ("name", duration, album_id)
-VALUES('song1_4', 442, 4);
-
-INSERT INTO public.song
-("name", duration, album_id)
-VALUES('song2_4', 352, 4);
+VALUES
+('song1_4', 442, 4),
+('song2_4', 352, 4);
 
 
 INSERT INTO public.collection
 (title, "year")
-VALUES('collection1', 2015);
-
-INSERT INTO public.collection
-(title, "year")
-VALUES('collection2', 2017);
-
-INSERT INTO public.collection
-(title, "year")
-VALUES('collection3', 2017);
-
-INSERT INTO public.collection
-(title, "year")
-VALUES('collection4', 2020);
+VALUES
+('collection1', 2015),
+('collection2', 2017),
+('collection3', 2017),
+('collection4', 2020);
 
 
 INSERT INTO public.catalog_song
 (song_id, collection_id)
-VALUES(1, 1);
-
-INSERT INTO public.catalog_song
-(song_id, collection_id)
-VALUES(2, 1);
-
-INSERT INTO public.catalog_song
-(song_id, collection_id)
-VALUES(3, 2);
-
-INSERT INTO public.catalog_song
-(song_id, collection_id)
-VALUES(4, 2);
-
-INSERT INTO public.catalog_song
-(song_id, collection_id)
-VALUES(5, 3);
-
-INSERT INTO public.catalog_song
-(song_id, collection_id)
-VALUES(6, 3);
-
-
-INSERT INTO public.catalog_song
-(song_id, collection_id)
-VALUES(7, 1);
-
-INSERT INTO public.catalog_song
-(song_id, collection_id)
-VALUES(8, 3);
+VALUES
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 2),
+(5, 3),
+(6, 3),
+(7, 1),
+(8, 3);
 
 
 INSERT INTO public.catalog_artist
 (artist_id, genre_id)
-VALUES(1, 1);
-INSERT INTO public.catalog_artist
-(artist_id, genre_id)
-VALUES(1, 2);
-
-INSERT INTO public.catalog_artist
-(artist_id, genre_id)
-VALUES(2, 2);
-
-INSERT INTO public.catalog_artist
-(artist_id, genre_id)
-VALUES(3, 3);
-
-INSERT INTO public.catalog_artist
-(artist_id, genre_id)
-VALUES(4, 4);
+VALUES
+(1, 1),
+(1, 2),
+(2, 2),
+(3, 3),
+(4, 4);
 
 
 INSERT INTO public.catalog_album
 (artist_id, album_id)
-VALUES(1, 1);
-
-INSERT INTO public.catalog_album
-(artist_id, album_id)
-VALUES(2, 2);
-
-INSERT INTO public.catalog_album
-(artist_id, album_id)
-VALUES(3, 3);
-
-INSERT INTO public.catalog_album
-(artist_id, album_id)
-VALUES(4, 4);
+VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
